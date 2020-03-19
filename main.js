401e3b022aa0131dc3d897d341aa868ec72d0754
Wandering Crow <FraternaliAlessandro@gmail.com>
2020-03-19 14:27:27 +0100

    Inserite var e prompt iniziali

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 // quanti km vuoi percorrere?
+var km = prompt("Quanti km vuoi percorrere?");
 
 // quanti anni hai?
+var age = prompt("Quanti anni hai?");
 
 // calcolo del prezzo in base ai km impostati (km x 0.21)
+var prezzo = km * 0.21;
 
 // sconto in base all'età (<18 20%, >65 40%)
+
+
+// coso html
+document.getElementById("costo").innerHTML = prezzo;
